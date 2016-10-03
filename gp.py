8fefb0af05b1790fc1ba6e60f95b57b5eaeae958
m133225 <m133225@hotmail.com>
2016-10-04 02:01:41 +0800

    Attempted to implement a simple GP and train the model using a subset (480) of data.

---
+++ gp.py
@@ -0,0 +1,32 @@
+import GPy
+import csv
+import numpy as np
+import time
+import plotly.plotly as py
+
+# pip install GPy
+# Roughly following GPy tutorial at http://nbviewer.jupyter.org/github/SheffieldML/notebook/blob/master/GPy/basic_gp.ipynb
+
+# Requires `pip install plotly` and sign up at plot.ly for api key
+py.sign_in('username', 'api_key')
+with open('data/subset_train.csv','rb') as data_file:
+    reader = csv.reader(data_file)
+    data = np.matrix(list(reader))[1:,:]
+    
+    features = data[:,1:].astype(np.float)
+    targets = data[:,0]
+    
+    # Convert time to epoch (a continuous value)
+    processed_targets = np.empty(targets.shape)
+    pattern = '%d/%m/%y %H:%M'
+    for i in range(len(targets)):
+        target_time = targets[i,0]
+        processed_targets[i,0] = float(time.mktime(time.strptime(target_time, pattern)))
+
+    print features
+    print processed_targets
+    m = GPy.models.GPRegression(features, processed_targets)
+
+    GPy.plotting.change_plotting_library('plotly')
+    fig = m.plot()
+    GPy.plotting.show(fig,  filename='basic_gp_regression_notebook')
