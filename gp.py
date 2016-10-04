4e32d2eb0f784a2966ef2d1a22b16e3f5e254bc8
m133225 <m133225@hotmail.com>
2016-10-04 12:15:31 +0800

    Some minor changes to plotly so that username and api_key does not need to be saved in the code.

--- gp.py
+++ gp.py
@@ -3,12 +3,14 @@ import csv
 import numpy as np
 import time
 import plotly.plotly as py
+import plotly
 
 # pip install GPy
 # Roughly following GPy tutorial at http://nbviewer.jupyter.org/github/SheffieldML/notebook/blob/master/GPy/basic_gp.ipynb
 
 # Requires `pip install plotly` and sign up at plot.ly for api key
-py.sign_in('username', 'api_key')
+# Need to execute the following once to set the credentials file locally; can be commented out after that
+# plotly.tools.set_credentials_file(username='username', api_key='api_key')
 with open('data/subset_train.csv','rb') as data_file:
     reader = csv.reader(data_file)
     data = np.matrix(list(reader))[1:,:]
