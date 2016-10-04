bec4dbbadd40ff79876949d7d17a89fbc595770e
m133225 <m133225@hotmail.com>
2016-10-04 21:57:01 +0800

    Fixed errors, but GP does not seem to be working.

--- gp.py
+++ gp.py
@@ -14,10 +14,11 @@ with open('data/subset_train.csv','rb') as train_file:
 
     m = GPy.models.GPRegression(features, targets)
 
+print "Model:", m
 
 with open('data/subset_test.csv','rb') as test_file:
     reader = csv.reader(test_file)
-    data = np.matrix(list(reader))[1:,:]
+    data = np.array(list(reader))[1:,:]
 
     features = data[:,1:].astype(np.int).astype(np.float)
     targets = data[:,0]
@@ -25,7 +26,6 @@ with open('data/subset_test.csv','rb') as test_file:
     print "Number of features in test data:", data.shape[1]
 
     for i in range(data.shape[0]):
-        print "Predicting for:", features[i]
-        mu, C, up95, lo95 = m.predict(features[i])
-        print "Predicted:", mu
+        mu, C = m.predict(np.array([features[i]]))
+        print "Predicted:", mu, C
         print "Actual:", targets[i]
