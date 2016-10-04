a49a2e11110d9f567c53579c60b8215ab1823609
m133225 <m133225@hotmail.com>
2016-10-04 20:15:00 +0800

    Cleanup gp.py to read the new input.

--- gp.py
+++ gp.py
@@ -15,20 +15,10 @@ with open('data/subset_train.csv','rb') as data_file:
     reader = csv.reader(data_file)
     data = np.matrix(list(reader))[1:,:]
     
-    features = data[:,1:].astype(np.float)
+    features = data[:,1:].astype(np.int).astype(np.float)
     targets = data[:,0]
     
-    # Convert time to epoch (a continuous value)
-    processed_targets = np.empty(targets.shape)
-    pattern = '%d/%m/%y %H:%M'
-    for i in range(len(targets)):
-        target_time = targets[i,0]
-        processed_targets[i,0] = float(time.mktime(time.strptime(target_time, pattern)))
-
+    
     print features
-    print processed_targets
-    m = GPy.models.GPRegression(features, processed_targets)
 
-    GPy.plotting.change_plotting_library('plotly')
-    fig = m.plot()
-    GPy.plotting.show(fig,  filename='basic_gp_regression_notebook')
+    m = GPy.models.GPRegression(features, targets)
