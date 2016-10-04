3fdecb1571149acc7535325da28a238a41225497
m133225 <m133225@hotmail.com>
2016-10-04 21:38:23 +0800

    Attempted implement prediction of target value using model.

--- gp.py
+++ gp.py
@@ -1,24 +1,31 @@
 import GPy
 import csv
 import numpy as np
-import time
-import plotly.plotly as py
-import plotly
 
 # pip install GPy
-# Roughly following GPy tutorial at http://nbviewer.jupyter.org/github/SheffieldML/notebook/blob/master/GPy/basic_gp.ipynb
-
-# Requires `pip install plotly` and sign up at plot.ly for api key
-# Need to execute the following once to set the credentials file locally; can be commented out after that
-# plotly.tools.set_credentials_file(username='username', api_key='api_key')
-with open('data/subset_train.csv','rb') as data_file:
-    reader = csv.reader(data_file)
+with open('data/subset_train.csv','rb') as train_file:
+    reader = csv.reader(train_file)
     data = np.matrix(list(reader))[1:,:]
     
     features = data[:,1:].astype(np.int).astype(np.float)
     targets = data[:,0]
     
-    
-    print features
+    print "Number of features in training data:", data.shape[1]
 
     m = GPy.models.GPRegression(features, targets)
+
+
+with open('data/subset_train.csv','rb') as test_file:
+    reader = csv.reader(test_file)
+    data = np.matrix(list(reader))[1:,:]
+
+    features = data[:,1:].astype(np.int).astype(np.float)
+    targets = data[:,0]
+    
+    print "Number of features in test data:", data.shape[1]
+
+    for i in range(data.shape[0]):
+        print "Predicting for:", features[i]
+        mu, C, up95, lo95 = m.predict(features[i])
+        print "Predicted:", mu
+        print "Actual:", targets[i]
