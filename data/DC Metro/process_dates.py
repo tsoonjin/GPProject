1f57b449eb47484876aa3ac94649a8d1c445fedd
m133225 <m133225@hotmail.com>
2016-10-04 16:51:50 +0800

    Added DC metro and codeodes to process the dates. Sorted the resulting records by reporting date.

---
+++ data/DC Metro/process_dates.py
@@ -0,0 +1,29 @@
+import GPy
+import csv
+import numpy as np
+import time
+import plotly.plotly as py
+import plotly
+
+# swaps the date and month columns (1,17,18) from the raw data
+with open('train.csv','rb') as data_file:
+    reader = csv.reader(data_file, delimiter=',')
+    data = list(reader)
+    no_of_data = len(data)
+    
+    pattern = '%m/%d/%Y %I:%M:%S %p'
+    for i in range(no_of_data - 1):
+        target_time = data[i + 1][0]
+        data[i + 1][0] = time.strftime('%d/%m/%Y %I:%M %p', time.strptime(target_time, pattern))
+
+        target_time = data[i + 1][16]
+        data[i + 1][16] = time.strftime('%d/%m/%Y %I:%M %p', time.strptime(target_time, pattern))
+        
+        target_time = data[i + 1][17]
+        if target_time is not "":
+            data[i + 1][17] = time.strftime('%d/%m/%Y %I:%M %p', time.strptime(target_time, pattern))
+
+
+with open('processed_train.csv','wb') as processed_train:
+    writer = csv.writer(processed_train, delimiter=',')
+    writer.writerows(data)
